662ba755e0b703cf40d961a90a7198c9fee02327
xuzhifa66 <18810869524@163.com>
2018-08-07 13:02:57 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,12 +1,12 @@
 import os
 import fileinput
 
-envVar = os.environ.get( "MYSQL_DATA_DIR" )
+envVar = os.environ.get( "MYSQL_BINLOG_DIR" )
 envVar =  str(envVar).rstrip('/') + '/'
 if envVar in 'None':
-    print 'MYSQL_DATA_DIR is null'
+    print 'MYSQL_BINLOG_DIR is null'
     exit()
-for line in fileinput.input('./testRestore.py',inplace=1):
+for line in fileinput.input('./mysqlTableRestore.py',inplace=1):
     if line.find('binlogDir =') != -1:
         print "binlogDir ='{}'".format(envVar)
     else:
